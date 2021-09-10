10cafeb0e44c374b6ffba628e045fe7d036b2228
A01252527 <a01252527@itesm.mx>
2021-09-10 17:24:36 +0000

    entrega

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,8 +1,20 @@
-
-
+import math 
 def main():
     # Escribe tu código abajo de esta línea
-   
+        radio = float(input('Introduce el radio: '))
+        x1 = float(input('Introduce x1: '))
+        y1 = float(input('Introduce y1: '))
+        x2 = float(input('Introduce x2: '))
+        y2 = float(input('Introduce y2: '))
+        d = math.sqrt(((x2-x1)**2)+(y2-y1)**2)
+        if d ==radio:
+            print(str('SOBRE'))
+        elif d <radio:
+            print(str('DENTRO'))
+        else: 
+            print(str('FUERA'))
+       
+
 
 if __name__ == '__main__':
     main()
